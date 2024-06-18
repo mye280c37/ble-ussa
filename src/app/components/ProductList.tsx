28ed92bb032a137929ff0cd0d37fe77a7e639607
mye280c37 <mye785c83@gmail.com>
2024-06-18 14:20:13 +0900

    Develop adding products fuction

--- src/app/components/ProductList.tsx
+++ src/app/components/ProductList.tsx
@@ -1,4 +1,5 @@
 
+import React, { useState } from 'react';
 import {
     Typography, Box,
     Grid,
@@ -12,15 +13,33 @@ import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import AddBoxIcon from '@mui/icons-material/AddBox';
 import IndeterminateCheckBoxIcon from '@mui/icons-material/IndeterminateCheckBox';
 import DashboardCard from '@/app/dashboard/components/shared/DashboardCard';
-import Products from '../../../data/products';
+import Products from '../../data/products';
+
+interface ShoppingItem {
+  [key: string]: number;
+}
 
 
 const ProductList = () => {
+    const parsedProducts = Products.map((product) => {
+        return {
+            ...product,
+            price: parseFloat(product.price),
+            quantity: parseInt(product.quantity)
+        }
+    });
+    const [shoppingCart, setShoppingCart] = useState<ShoppingItem>(()=>{
+      var cart:ShoppingItem = {};
+      Products.map((product) => {
+        cart[product.id] = 0;
+      });
+      return cart;
+    });
     return (
         <Box sx={{ height: "100%"}}>
-            <DashboardCard title="상품 목록" height='80dvh'>
+            <DashboardCard title="상품 목록" height='95%'>
                 <Box sx={{ overflow: 'auto', width: 'auto', pt: 3 }}>
-                    {Products.map((product) => (
+                    {parsedProducts.map((product) => (
                         <Box sx={{ pb: 3 }} key={product.id}>
                             <Grid container spacing={3}>
                                 <Grid item xs={6} lg={3}>
@@ -84,7 +103,20 @@ const ProductList = () => {
                                 </Grid>
                                 <Grid item xs={6} lg={3}>
                                     <Box sx={{ display: "flex", justifyContent: "center" }}>
-                                        <IconButton color="secondary" sx={{ pl: 0, pr: 0}} aria-label="remove from shopping cart">
+                                        <IconButton 
+                                          color="secondary" 
+                                          sx={{ pl: 0, pr: 0}} 
+                                          aria-label="remove from shopping cart"
+                                          onClick={() => {
+                                            if (shoppingCart[product.id] === 0) {
+                                              return;
+                                            }
+                                            setShoppingCart({
+                                              ...shoppingCart,
+                                              [product.id]: shoppingCart[product.id] - 1
+                                            });
+                                          }}
+                                        >
                                             <IndeterminateCheckBoxIcon />
                                         </IconButton>
                                         <TextField 
@@ -93,8 +125,36 @@ const ProductList = () => {
                                             sx={{ width: "48px" }}
                                             size="small" 
                                             defaultValue={0}
+                                            value={shoppingCart[product.id]}
+                                            onChange={() => {
+                                              if (shoppingCart[product.id] < 0) {
+                                                setShoppingCart({
+                                                  ...shoppingCart,
+                                                  [product.id]: 0
+                                                });
+                                              }
+                                              if (shoppingCart[product.id] > product.quantity) {
+                                                setShoppingCart({
+                                                  ...shoppingCart,
+                                                  [product.id]: product.quantity
+                                                });
+                                              }
+                                            }}
                                         />
-                                        <IconButton color="primary" sx={{ pl: 0, pr: 0}} aria-label="add to shopping cart">
+                                        <IconButton 
+                                          color="primary" 
+                                          sx={{ pl: 0, pr: 0}} 
+                                          aria-label="add to shopping cart"
+                                          onClick={() => {
+                                            if (shoppingCart[product.id] === product.quantity) {
+                                              return;
+                                            }
+                                            setShoppingCart({
+                                              ...shoppingCart,
+                                              [product.id]: shoppingCart[product.id] + 1
+                                            });
+                                          }}
+                                        >
                                             <AddBoxIcon />
                                         </IconButton>
                                         <IconButton color="primary" aria-label="add to shopping cart" sx={{ pr:0 }}>
@@ -108,10 +168,6 @@ const ProductList = () => {
                     ))}
                 </Box>
             </DashboardCard>
-            {/* <Box sx={{ height: "6%", textAlign: "right" }}>
-            <Button variant="contained">결제</Button>
-            </Box> */}
-            <Button variant="contained" sx={{ position: "absolute", right: 10, bottom: 10 }}>결제</Button>
         </Box>
     );
 };
